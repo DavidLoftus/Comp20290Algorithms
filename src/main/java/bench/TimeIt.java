fad8f6d057b940a4fd6ba09b5b2ecf198cf17e4c
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2020-04-09 16:05:55 +0100

    TimeIt updates

---
+++ src/main/java/bench/TimeIt.java
@@ -0,0 +1,48 @@
+package bench;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class TimeIt<T> {
+
+    private Supplier<T> setup = null;
+    private Consumer<T> func;
+
+    public TimeIt(Supplier<T> setup, Consumer<T> func) {
+        this.func = func;
+        this.setup = setup;
+    }
+
+    public static <T> long run(long minNano, Supplier<T> setup, Consumer<T> func) {
+        TimeIt<T> timer = new TimeIt<>(setup, func);
+        return timer.run(minNano);
+    }
+
+    public static long run(long minNano, Runnable setup, Runnable func) {
+        return run(minNano, () -> {setup.run(); return null;}, __ -> func.run());
+    }
+
+    public long run() {
+
+        T setupVar = setup.get();
+
+        long before = System.nanoTime();
+
+        func.accept(setupVar);
+
+        long after = System.nanoTime();
+
+        return after - before;
+    }
+
+    public long run(long minNano) {
+        long sum = 0;
+        int iters = 0;
+        while(sum < minNano) {
+            sum += run();
+            iters++;
+        }
+        return sum / iters;
+    }
+
+}
