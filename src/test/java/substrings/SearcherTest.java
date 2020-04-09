73408dffa923bed5de193a95518fef07a1d3f342
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2020-04-09 15:05:52 +0100

    Tests for Searcher.findFirst

---
+++ src/test/java/substrings/SearcherTest.java
@@ -0,0 +1,21 @@
+package substrings;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SearcherTest {
+    @ParameterizedTest
+    @ValueSource(classes = {BruteForceSearch.class, KMPSearch.class})
+    void findFirst(Class<? extends Searcher> searcherClass) throws ReflectiveOperationException {
+        Searcher searcher = searcherClass.getConstructor().newInstance();
+
+        assertEquals(0, searcher.findFirst("Hello World", "Hello World"));
+        assertEquals(6, searcher.findFirst("Hello World", "Wor"));
+        assertEquals(4, searcher.findFirst("Hel World", "Wor"));
+
+        assertEquals(-1, searcher.findFirst("Hello World", "Goodbye World"));
+        assertEquals(-1, searcher.findFirst("Hello World", "Hello Planet"));
+    }
+}
