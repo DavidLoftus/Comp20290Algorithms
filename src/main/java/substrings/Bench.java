a60358fc2568e50c7b285475131fa5e41c4f0143
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2020-04-09 15:24:55 +0100

    String search benchmark brute force is faster no matter what, need to fix

---
+++ src/main/java/substrings/Bench.java
@@ -0,0 +1,30 @@
+package substrings;
+
+import bench.TimeIt;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.TimeUnit;
+
+public class Bench {
+
+    public static void main(String[] args) throws IOException {
+        InputStream inputStream = Bench.class.getResourceAsStream("moby10b.txt");
+        String largeString = new String(inputStream.readAllBytes());
+
+        System.out.println("findFirst:");
+        for (Searcher searcher : new Searcher[]{new BruteForceSearch(), new KMPSearch()}) {
+            long oneSecondNanos = TimeUnit.SECONDS.toNanos(1);
+            long time = TimeIt.run(oneSecondNanos, () -> {}, () -> searcher.findAll(largeString, "and then"));
+            System.out.printf("\t%s\t%.03fms\n", searcher.getClass().getSimpleName(), time / 1000000.0);
+        }
+
+        System.out.println("findFirst:");
+        for (Searcher searcher : new Searcher[]{new BruteForceSearch(), new KMPSearch()}) {
+            long oneSecondNanos = TimeUnit.SECONDS.toNanos(1);
+            long time = TimeIt.run(oneSecondNanos, () -> {}, () -> searcher.findFirst(largeString, "and then"));
+            System.out.printf("\t%s\t%.03fms\n", searcher.getClass().getSimpleName(), time / 1000000.0);
+        }
+    }
+
+}
