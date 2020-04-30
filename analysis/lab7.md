b50e98efdb25fd3075a9f64de33bb4c14bb87403
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2020-05-01 00:36:12 +0100

    lab 7 writeup

--- analysis/lab7.md
+++ analysis/lab7.md
@@ -1,1 +1,31 @@
 # Practical 7: String search algorithms
+
+### Algorithmic Development
+
+##### Brute Force
+
+See
+[BruteForceSearch.java](../src/main/java/ie/davidloftus/algorithms/substrings/BruteForceSearch.java)
+for brute force string search implementation.
+
+##### KMP Search
+
+See
+[KMPSearch.java](../src/main/java/ie/davidloftus/algorithms/substrings/KMPSearch.java)
+for KMP (Knuth-Morris-Pratt) string search implementation.
+
+##### Performance Analysis
+
+Here is the benchmarks for the search algorithms using different testcases.
+You can find the exact value for these testcases
+
+|                   Description |    BruteForceSearch |           KMPSearch |
+|-------------------------------|---------------------|---------------------|
+|       tiny string not present |              0.0025 |              0.0026 |
+|    small string, present, end |              0.0027 |              0.0026 |
+|repeated string, short circuit |              0.0026 |              0.0025 |
+|           100 false positives |              0.0291 |              0.0194 |
+|super long needle and haystack |            635.7659 |              4.5062 |
+
+As you can see for small strings brutre force is marginally better, but KMP quickly becomes the more optimal algorithm.
+It is very apparent in the final test case where both the needle and haystack are long.
--- src/main/java/ie/davidloftus/algorithms/substrings/Bench.java
+++ src/main/java/ie/davidloftus/algorithms/substrings/Bench.java
@@ -4,28 +4,57 @@ import ie.davidloftus.algorithms.bench.TimeIt;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.zip.GZIPInputStream;
 
 public class Bench {
 
+    private static class Testcase {
+        String needle;
+        String haystack;
+
+        String description;
+
+        public Testcase(String needle, String haystack, String description) {
+            this.needle = needle;
+            this.haystack = haystack;
+            this.description = description;
+        }
+
+        public Testcase(String needle, String haystack) {
+            this(needle, haystack, needle + ":" + haystack);
+        }
+    }
+
+    private static final List<Searcher> searchers = List.of(new BruteForceSearch(), new KMPSearch());
+
+    public static void runTestCase(Testcase tc) {
+        System.out.printf("|%30s |", tc.description);
+        for (Searcher searcher : searchers) {
+            long oneSecondNanos = TimeUnit.MILLISECONDS.toNanos(100);
+            long time = TimeIt.run(oneSecondNanos, () -> {}, () -> searcher.findFirst(tc.haystack, tc.needle));
+            System.out.printf("%20.4f |", time / 1000000.0);
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) throws IOException {
-        InputStream inputStream = new GZIPInputStream(Bench.class.getResourceAsStream("moby10b.txt.gz"));
-        String largeString = new String(inputStream.readAllBytes());
+        List<Testcase> testcases = List.of(
+                new Testcase("needle", "haystack", "tiny string not present"),
+                new Testcase("character", "kinda big string with lots of characters", "small string, present, end"),
+                new Testcase("character", "kinda big string with lots of characters".repeat(10), "repeated string, short circuit"),
+                new Testcase("racecar", "racec".repeat(1000) + "racecar", "100 false positives"),
+                new Testcase("e".repeat(256) + "yyy!", "e".repeat(1024*1024) + "yyy!", "super long needle and haystack")
+                );
 
-        System.out.println("findFirst:");
-        for (Searcher searcher : new Searcher[]{new BruteForceSearch(), new KMPSearch()}) {
-            long oneSecondNanos = TimeUnit.SECONDS.toNanos(1);
-            long time = TimeIt.run(oneSecondNanos, () -> {}, () -> searcher.findAll(largeString, "and then"));
-            System.out.printf("\t%s\t%.03fms\n", searcher.getClass().getSimpleName(), time / 1000000.0);
+        System.out.printf("|%30s |", "Description");
+        for (Searcher searcher : searchers) {
+            System.out.printf("%20s |", searcher.getClass().getSimpleName());
         }
-
-        System.out.println("findFirst:");
-        for (Searcher searcher : new Searcher[]{new BruteForceSearch(), new KMPSearch()}) {
-            long oneSecondNanos = TimeUnit.SECONDS.toNanos(1);
-            long time = TimeIt.run(oneSecondNanos, () -> {}, () -> searcher.findFirst(largeString, "and then"));
-            System.out.printf("\t%s\t%.03fms\n", searcher.getClass().getSimpleName(), time / 1000000.0);
-        }
+        System.out.println();
+        System.out.println("|" + "-".repeat(31) + "|" + "---------------------|".repeat(searchers.size()));
+        testcases.forEach(Bench::runTestCase);
     }
 
 }
